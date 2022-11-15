62963b7ebcc980d20129c7f3ea1f290e22e0cf65
Ilya <htctomsk634@gmail.com>
2022-11-15 18:37:11 +0700

    Create CreatePhoneNumber.py

---
+++ CreatePhoneNumber.py
@@ -0,0 +1,13 @@
+def create_phone_number(n):
+    #your code here
+    stri = "({prefix}) {fix}-{main}"
+    prefix = ""
+    fix = ""
+    main = ""
+    for i in range(3):
+        prefix += str(n[i])
+    for i in range(3,6):
+        fix += str(n[i])
+    for i in range(6,10):
+        main += str(n[i])
+    return stri.format(prefix=prefix, fix=fix, main=main)
