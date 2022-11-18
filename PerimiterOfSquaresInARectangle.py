fddc91ba165e93364dd3562bbedebb6a6ceee33b
Ilya <htctomsk634@gmail.com>
2022-11-18 19:20:35 +0700

    Create PerimiterOfSquaresInARectangle.py

---
+++ PerimiterOfSquaresInARectangle.py
@@ -0,0 +1,10 @@
+def perimeter(n):
+    return 4*fib_sum(n+2)
+    
+def fib(n, computed = {0: 0, 1: 1}):
+    if n not in computed:
+        computed[n] = fib(n-1, computed) + fib(n-2, computed)
+    return computed[n]
+
+def fib_sum(n):
+    return sum(fib(i) for i in range(n))
