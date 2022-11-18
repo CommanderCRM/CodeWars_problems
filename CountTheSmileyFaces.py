3c7cef169bb7866ce30f8c2186af8bc5eee3e209
Ilya <htctomsk634@gmail.com>
2022-11-18 18:48:26 +0700

    Create CountTheSmileyFaces.py

---
+++ CountTheSmileyFaces.py
@@ -0,0 +1,9 @@
+import re
+
+def count_smileys(arr):
+    count = 0
+    regex = r"[:;][-~]?[)D]"
+    for string in arr:
+        if re.search(regex, string):
+            count += 1
+    return count
