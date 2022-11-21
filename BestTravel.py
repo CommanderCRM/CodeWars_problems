7424852ba47e0d65a8d362d80708f70c9cd20222
Ilya <htctomsk634@gmail.com>
2022-11-21 22:35:47 +0700

    Create BestTravel.py

---
+++ BestTravel.py
@@ -0,0 +1,8 @@
+from itertools import combinations
+
+def choose_best_sum(t, k, ls):
+    arr = list(combinations(ls, k))
+    arr = [sum(i) for i in arr if sum(i) <= t]
+    if len(arr) == 0:
+        return None
+    return max(arr)
